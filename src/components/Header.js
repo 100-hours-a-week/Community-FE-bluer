555634f4469c34e763230626cbf3b0d6c585fed9
kkan9ma <kyeongmankang@gmail.com>
2025-11-09 02:33:40 +0900

    헤더 조건부 렌더링을 위한 UI 마크업 작업

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,44 @@ import { getState } from "../lib/store.js";
 
 function Header({ $target, initialState }) {
   this.target = $target;
-  this.state = { ...initialState, isLoggedIn: false };
+  this.state = { ...initialState };
   this.$header = document.createElement("header");
+  this.$header.classList.add("header");
 
   this.setState = newState => {
     this.state = { ...this.state, ...newState };
   };
 
+  /*
+    1. 로그인한 상태 -> 우측 유저 드롭다운 버튼
+    2. 게시글 상세조회, 게시글 수정, 게시글 추가 페이지, 회원가입 -> 뒤로가기
+  */
+
+  this.renderBackButton = () => {};
+  this.renderDropdownButton = () => {};
+
   this.render = () => {
     // TODO: 로그인 상태에 따른 조건부 렌더링
     this.$header.classList.add("header");
     this.$header.innerHTML = `
-      <h1>아무 말 대잔치 :${this.state.isLoggedIn ? "로그인" : "로그인 안 함"}</h1>
+      <div class="header-content">
+         <div class="header-back-button-container"> 
+            <button>
+              <img
+                style="height:100%"
+                src="./public/left-arrow.png"
+              />
+            </button>
+          </div>
+          <span class="header-title bold">아무 말 대잔치</span>
+          <button class="dropdown-button">
+            <div class="avatar">
+              <img src="./public/profile-sample.jpeg" /> 
+            </div>
+          </button>
+        </div>
+      </div>
+      
       `;
     this.target.appendChild(this.$header);
   };
