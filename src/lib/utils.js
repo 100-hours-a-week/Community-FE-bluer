7b35338720ccaacc2880ce8faa66470cd981aa10
kkan9ma <kyeongmankang@gmail.com>
2025-11-10 16:35:23 +0900

    Modal 렌더링할 수 있도록 Index.html 구조 수정 및 호출 함수 구현

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -18,3 +18,55 @@ export const showToast = (message, duration = 3000) => {
     toast.remove();
   }, duration);
 };
+
+export const showModal = ({
+  modalTitle,
+  modalDescription,
+  onPositive,
+  onNegative,
+  positiveText,
+  negativeText,
+}) => {
+  const $modal = document.getElementById("modal");
+
+  $modal.innerHTML = "";
+
+  $modal.innerHTML = `
+  <div class="overlay"></div>
+    <div class="modal-wrapper">
+      <div class="modal-content">
+        <h2 class="modal-title bold">${modalTitle}</h2>
+        <p class="modal-description">${modalDescription}</p>
+        <div class="modal-actions">
+          <button class="negative-button">${negativeText}</button>
+          <button class="positive-button">${positiveText}</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  const $overlay = $modal.querySelector(".overlay");
+  const $positiveButton = $modal.querySelector(".positive-button");
+  const $negativeButton = $modal.querySelector(".negative-button");
+
+  $modal.classList.remove("hidden");
+
+  const close = () => {
+    $modal.classList.add("hidden");
+    $modal.innerHTML = ""; // 내부 완전히 비워서 다음 호출 시 새로 생성되도록
+  };
+
+  const handlePositive = () => {
+    onPositive && onPositive();
+    close();
+  };
+
+  const handleNegative = () => {
+    onNegative && onNegative();
+    close();
+  };
+
+  $positiveButton.addEventListener("click", handlePositive);
+  $negativeButton.addEventListener("click", handleNegative);
+  $overlay.addEventListener("click", handleNegative);
+};
