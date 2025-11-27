a0fa281a9bd2e7b1f1bfeed66e3d090e31358d62
kkan9ma <kyeongmankang@gmail.com>
2025-11-27 21:38:21 +0900

    프로젝트 초기 세팅: React with JavaScript(vite), react-router-dom, TailwindCSS, ESLint

---
+++ react/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Button from "./components/ui/Button";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="app">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Button />}></Route>
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</>
+	);
+}
+
+export default App;
