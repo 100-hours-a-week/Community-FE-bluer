6c3044198a85ac9aa7b2884493be02aeac559f52
kkan9ma <kyeongmankang@gmail.com>
2025-11-27 22:12:01 +0900

    ESLint, Prettier 설정 및 import rule 적용

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Button from "./components/ui/Button";
+import Button from "@/components/ui/Button";
+
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<div className="app">
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Button />}></Route>
-					</Routes>
-				</BrowserRouter>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Button />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- react/vite.config.js
+++ react/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindCSS from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindCSS()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
