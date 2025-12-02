6a19937a3e91b862cd6e8d7507d44e35c421bf0c
kkan9ma <kyeongmankang@gmail.com>
2025-12-02 16:24:10 +0900

    Button 컴포넌트 구현

--- react/src/components/ui/Button.jsx
+++ react/src/components/ui/Button.jsx
@@ -1,13 +1,41 @@
-import { useState } from "react";
+import { cva } from "class-variance-authority";
+import { cn } from "@/utils/cn";
 
-function Button() {
-	const [count, setCount] = useState(0);
+const buttonStyles = cva(
+  "inline-flex items-center justify-center rounded-md font-medium transition disabled:cursor-not-allowed",
+  {
+    variants: {
+      variant: {
+        primary: "bg-button-bg-primary text-button-text-primary border-none",
+        secondary:
+          "bg-button-bg-secondary  text-button-text-secondary border border-solid border-black",
+      },
+      disabled: {
+        true: "",
+        false: "",
+      },
+      size: {
+        sm: "px-3 py-2 text-sm",
+        md: "px-4 py-2 text-md",
+        lg: "px-5 py-3 text-lg",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+      disabled: false,
+    },
+  }
+);
 
-	return (
-		<button onClick={() => setCount((count) => count + 1)}>
-			count is {count}
-		</button>
-	);
+export default function Button({ variant, disabled, size, className, children, ...props }) {
+  return (
+    <button
+      className={cn(buttonStyles({ variant, disabled, size }), className)}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
-
-export default Button;
