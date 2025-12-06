da9c10b8b520675bb307540943a5d0ed5542b95a
kkan9ma <kyeongmankang@gmail.com>
2025-12-07 01:59:18 +0900

    프로필 이미지 변경 기능 구현

--- react/src/pages/UserInfoPage.jsx
+++ react/src/pages/UserInfoPage.jsx
@@ -1,24 +1,50 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import useFileInput from "@/hooks/useFileInput";
 import useClearUserContext from "@/contexts/useClearUserContext";
 import useLoggedInUser from "@/contexts/useLoggedInUser";
+import useRefreshUser from "@/contexts/useRefreshUser";
 import { apiManager } from "@/lib/api/apiManager";
+import { uploadToImageBucket } from "@/lib/extenal/imageBucket";
 import UserInfoPageMenuItem from "@/components/page/UserInfoPage/UserInfoPageItem";
 import Avatar from "@/components/ui/avatar";
 import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
 import Text from "@/components/ui/Text";
 
 function UserInfoPage() {
   const { user } = useLoggedInUser();
   const { clearUserInfo } = useClearUserContext();
+  const { refreshUserInfo } = useRefreshUser();
 
   const navigate = useNavigate();
   const location = useLocation();
 
+  const changeProfileImage = async (file) => {
+    // TODO: toast or error
+
+    let uploadedImageUrl;
+    try {
+      uploadedImageUrl = await uploadToImageBucket(file);
+    } catch (error) {
+      console.error(error);
+    }
+
+    try {
+      await apiManager.updateProfile({ profileImageUrl: uploadedImageUrl });
+
+      refreshUserInfo();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { inputRef, onClickFileInput, onChange } = useFileInput({
+    onFileChangeSuccess: changeProfileImage,
+  });
+
   const logout = async () => {
     try {
-      const result = await apiManager.signOut();
-
-      console.log(result);
+      await apiManager.signOut();
 
       clearUserInfo();
       navigate("/");
@@ -38,9 +64,20 @@ function UserInfoPage() {
           </div>
         </div>
         <div className="flex w-full gap-x-2 pt-1 pb-3">
-          <Button variant="secondary" className="border-button-secondary-border h-9 w-full">
+          <Button
+            variant="secondary"
+            className="border-button-secondary-border h-9 w-full"
+            onClick={onClickFileInput}
+          >
             <Text className="mx-auto">프로필 이미지 변경</Text>
           </Button>
+          <Input
+            onChange={onChange}
+            ref={inputRef}
+            type="file"
+            className="hidden"
+            accept="image/png, image/jpeg"
+          />
           <Button
             as={Link}
             to={`${location.pathname}/change-nickname`}
