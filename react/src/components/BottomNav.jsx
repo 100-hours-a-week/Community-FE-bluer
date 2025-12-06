ed727dfaba0fe7ef2418b573639289f674d4f357
kkan9ma <kyeongmankang@gmail.com>
2025-12-07 02:25:57 +0900

    Path 따라 BottomNav 다르게 렌더링하는 로직 구현

--- react/src/components/BottomNav.jsx
+++ react/src/components/BottomNav.jsx
@@ -1,50 +1,18 @@
-import { faPlus, faUser, faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { Link } from "react-router-dom";
+import { useCallback } from "react";
 import useRouteName from "@/hooks/route/useRouteName";
-import IconButton from "@/components/ui/IconButton";
-
-function BottomNavLinkButton(props) {
-  const { targetName, to, icon } = props;
-
-  const routeName = useRouteName();
-
-  return (
-    <IconButton
-      className={`flex-col gap-1 px-6 py-2 transition-all duration-300 ${
-        routeName === targetName ? "text-(--color-base-black)" : "text-gray-400"
-      }`}
-      as={Link}
-      to={to}
-    >
-      <FontAwesomeIcon icon={icon} size="xl" />
-    </IconButton>
-  );
-}
+import DefaultBottomNav from "@/components/bottomNav/DefaultBottomNav";
 
 function BottomNav() {
-  return (
-    <nav className="border-border-grey border-t-2 bg-white shadow-lg">
-      <ul className="mx-auto flex h-16 max-w-lg items-center justify-around px-4">
-        <li className="flex flex-1 justify-center">
-          <BottomNavLinkButton targetName={"post-list"} to={"/"} icon={faHome} />
-        </li>
+  const name = useRouteName();
 
-        <li className="flex flex-1 justify-center">
-          {/* TODO: 비회원이 클릭 시 모달 또는 로그인 */}
-          <IconButton className="bg-button-bg-hover h-14 w-14" as={Link} to={"/posts/create"}>
-            <FontAwesomeIcon icon={faPlus} size="lg" className="text-gray-400" />
-          </IconButton>
-        </li>
+  const renderHeaderByPath = useCallback(() => {
+    if (name === "post-detail") {
+      return <></>;
+    }
+    return <DefaultBottomNav />;
+  }, [name]);
 
-        <li className="flex flex-1 justify-center">
-          {/* TODO: 비회원이 클릭 시 모달 또는 로그인 */}
-          <BottomNavLinkButton targetName={"user-info"} to={"/user/info"} icon={faUser} />
-        </li>
-      </ul>
-    </nav>
-  );
+  return <>{renderHeaderByPath()}</>;
 }
 
 export default BottomNav;
